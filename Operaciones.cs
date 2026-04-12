435f27cbbd78a6b8fb43cedb77288ed6016932fe
AdAstraAbyssosque67 <jg2024-0823@uce.edu.do>
2026-04-12 19:30:36 -0400

    Add files via upload

---
+++ Operaciones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class Operaciones
+{
+    public static int Sumar(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Restar(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiplicar(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Dividir(int a, int b)
+    {
+        if (a == 0 || b == 0)
+        {
+            Console.WriteLine("Error: No se puede dividir entre cero.");
+            return 0;
+        }
+
+        return a / b;
+    }
+}
